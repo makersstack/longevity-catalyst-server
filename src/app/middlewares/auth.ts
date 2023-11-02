be9d7e90daf03e1175c660380631ef7dcb02e1d7
Shihab <73121069+maker-shihab@users.noreply.github.com>
2023-11-02 23:00:32 +0600

    Update Request overwrite

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -1,34 +1,35 @@
-// import { NextFunction, Request, Response } from "express";
-// import httpStatus from "http-status";
-// import { Secret } from "jsonwebtoken";
-// import config from "../../config";
-// import ApiError from "../../errors/ApiError";
-// import { jwtHelpers } from "../../helpers/jwtHelpers";
+import { NextFunction, Request, Response } from "express";
+import httpStatus from "http-status";
+import { Secret } from "jsonwebtoken";
+import config from "../../config";
+import ApiError from "../../errors/ApiError";
+import { jwtHelpers } from "../../helpers/jwtHelpers";
 
-// const auth =
-//   (...requiredRoles: string[]) =>
-//   async (req: Request, res: Response, next: NextFunction) => {
-//     try {
-//       //get authorization token
-//       const token = req.headers.authorization;
-//       if (!token) {
-//         throw new ApiError(httpStatus.UNAUTHORIZED, "You are not authorized");
-//       }
-//       // verify token
-//       let verifiedUser = null;
+const auth =
+  (...requiredRoles: string[]) =>
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      //get authorization token
+      const token = req.headers.authorization;
+      if (!token) {
+        throw new ApiError(httpStatus.UNAUTHORIZED, "You are not authorized");
+      }
 
-//       verifiedUser = jwtHelpers.verifyToken(token, config.jwt.secret as Secret);
+      // verify token
+      let verifiedUser = null;
 
-//       req.user = verifiedUser; // role  , userid
+      verifiedUser = jwtHelpers.verifyToken(token, config.jwt.secret as Secret);
 
-//       // role diye guard korar jnno
-//       if (requiredRoles.length && !requiredRoles.includes(verifiedUser.role)) {
-//         throw new ApiError(httpStatus.FORBIDDEN, "Forbidden");
-//       }
-//       next();
-//     } catch (error) {
-//       next(error);
-//     }
-//   };
+      req.user = verifiedUser; // role  , userid
 
-// export default auth;
+      // role diye guard korar jnno
+      if (requiredRoles.length && !requiredRoles.includes(verifiedUser.role)) {
+        throw new ApiError(httpStatus.FORBIDDEN, "Forbidden");
+      }
+      next();
+    } catch (error) {
+      next(error);
+    }
+  };
+
+export default auth;
