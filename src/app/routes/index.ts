fb63d39ecb652649b44c97451908762279b0114f
Shihab <73121069+maker-shihab@users.noreply.github.com>
2023-11-15 00:41:54 +0600

    Update for bug fixing

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,12 +1,7 @@
 import express from "express";
 import { AdminRoute } from "../modules/admin/admin.router";
 import { AuthRoutes } from "../modules/auth/auth.route";
-import { commentRoute } from "../modules/projects/comments/comments.route";
-import { ProjectRouter } from "../modules/projects/project.route";
-import { replyRoute } from "../modules/projects/reply/reply.route";
 import { imageRoute } from "../modules/user/imageController";
-import { userSkillRoute } from "../modules/user/user-skills/skills.route";
-import { userSocailRoute } from "../modules/user/user-socail/socail.router";
 import { UserRoutes } from "../modules/user/user.router";
 
 const router = express.Router();
@@ -23,26 +18,26 @@ const moduleRoutes = [
     path: "/auth",
     route: AuthRoutes,
   },
-  {
-    path: "/skills",
-    route: userSkillRoute,
-  },
-  {
-    path: "/socail",
-    route: userSocailRoute,
-  },
-  {
-    path: "/projects",
-    route: ProjectRouter,
-  },
-  {
-    path: "/comment",
-    route: commentRoute,
-  },
-  {
-    path: "/reply",
-    route: replyRoute,
-  },
+  // {
+  //   path: "/skills",
+  //   route: userSkillRoute,
+  // },
+  // {
+  //   path: "/socail",
+  //   route: userSocailRoute,
+  // },
+  // {
+  //   path: "/projects",
+  //   route: ProjectRouter,
+  // },
+  // {
+  //   path: "/comment",
+  //   route: commentRoute,
+  // },
+  // {
+  //   path: "/reply",
+  //   route: replyRoute,
+  // },
   {
     path: "/upload",
     route: imageRoute,
