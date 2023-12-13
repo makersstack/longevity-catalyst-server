1d460c19b335e7e3895d5d158d69da57ab570f90
Alifur Rahman <alifurrahman1999@gmail.com>
2023-12-13 15:42:23 +0600

    category filter sidebar

--- src/app/modules/projects/categories/categories.controller.ts
+++ src/app/modules/projects/categories/categories.controller.ts
@@ -1,8 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status";
-import ApiError from "../../../../errors/ApiError";
-import { utilities } from "../../../../helpers/utilities";
 import catchAsync from "../../../../shared/catchAsync";
 import sendResponse from "../../../../shared/sendResponse";
 import { CategoriesService } from "./categories.services";
@@ -10,14 +8,14 @@ import { CategoriesService } from "./categories.services";
 const getAllCategories = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
     const token = req.headers.authorization;
-    if (!token) {
-      throw new ApiError(httpStatus.UNAUTHORIZED, "Unauthorized!");
-    }
+    // if (!token) {
+    //   throw new ApiError(httpStatus.UNAUTHORIZED, "Unauthorized!");
+    // }
 
-    const isAuthorized = utilities.verifiedTokenAndDb(token);
-    if (!isAuthorized) {
-      throw new ApiError(httpStatus.UNAUTHORIZED, "Unauthorized!");
-    }
+    // const isAuthorized = utilities.verifiedTokenAndDb(token);
+    // if (!isAuthorized) {
+    //   throw new ApiError(httpStatus.UNAUTHORIZED, "Unauthorized!");
+    // }
 
     const result = await CategoriesService.getAllCategories();
 
